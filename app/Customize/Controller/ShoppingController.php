8d66de834835d362ba0132121f2b46aad98bb0b7
DESKTOP-D7HPFBN\WintShweSin <scm.wintshwesin@gmail.com>
2022-05-06 11:52:46 +0630

    Create CSV file in project folder

---
+++ app/Customize/Controller/ShoppingController.php
@@ -0,0 +1,191 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Customize\Controller;
+
+use Eccube\Controller\ShoppingController as baseController;
+use Eccube\Event\EccubeEvents;
+use Eccube\Event\EventArgs;
+use Eccube\Service\OrderHelper;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Eccube\Entity\Master\CsvType;
+use Eccube\Entity\ExportCsvRow;
+use Eccube\Service\CsvExportService;
+use Eccube\Repository\OrderRepository;
+use Eccube\Service\CartService;
+use Eccube\Service\MailService;
+
+class ShoppingController extends baseController
+{
+    /**
+     * @var CsvExportService
+     */
+    protected $csvExportService;
+    
+    /**
+     * @var CartService
+     */
+    protected $cartService;
+
+    /**
+     * @var MailService
+     */
+    protected $mailService;
+
+    /**
+     * @var OrderHelper
+     */
+    protected $orderHelper;
+
+    /**
+     * @var OrderRepository
+     */
+    protected $orderRepository;
+
+    public function __construct(
+        CartService $cartService,
+        MailService $mailService,
+        OrderRepository $orderRepository,
+        OrderHelper $orderHelper,
+        CsvExportService $csvExportService
+    ) {
+        $this->cartService = $cartService;
+        $this->mailService = $mailService;
+        $this->orderRepository = $orderRepository;
+        $this->orderHelper = $orderHelper;
+        $this->csvExportService = $csvExportService;
+    }
+
+    /**
+     * 購入完了画面を表示する.
+     *
+     * @Route("/shopping/complete", name="shopping_complete", methods={"GET"})
+     * @Template("Shopping/complete.twig")
+     */
+    public function complete(Request $request)
+    {
+        log_info('[注文完了] 注文完了画面を表示します.');
+
+        // 受注IDを取得
+        $orderId = $this->session->get(OrderHelper::SESSION_ORDER_ID);
+
+        if (empty($orderId)) {
+            log_info('[注文完了] 受注IDを取得できないため, トップページへ遷移します.');
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        $Order = $this->orderRepository->find($orderId);
+
+        $event = new EventArgs(
+            [
+                'Order' => $Order,
+            ],
+            $request
+        );
+        $this->eventDispatcher->dispatch(EccubeEvents::FRONT_SHOPPING_COMPLETE_INITIALIZE, $event);
+
+        if ($event->getResponse() !== null) {
+            return $event->getResponse();
+        }
+
+        log_info('[注文完了] 購入フローのセッションをクリアします. ');
+        $this->orderHelper->removeSession();
+
+        $hasNextCart = !empty($this->cartService->getCarts());
+
+        $order_no = $Order['id'];
+
+        //プロジェクトフォルダにCSVファイルを作成.
+        // タイムアウトを無効にする.
+        set_time_limit(0);
+
+        // sql loggerを無効にする.
+        $em = $this->entityManager;
+        $em->getConfiguration()->setSQLLogger(null);
+
+        $response = new StreamedResponse();
+        $csv_no = 1 ;
+        $this->csvExportService->getCsvNo($csv_no);
+
+        $response->setCallback(function () use ($request, $order_no) {
+            // CSV種別を元に初期化.
+            $this->csvExportService->initCsvType(CsvType::CSV_TYPE_USER_ORDER);
+
+            // ヘッダ行の出力.
+            $this->csvExportService->exportHeader();
+
+            // 受注データ検索用のクエリビルダを取得.
+            $qb = $this->csvExportService
+                ->getUserOrder($order_no);
+            
+
+            // データ行の出力.
+            $this->csvExportService->setExportQueryBuilder($qb);
+            $this->csvExportService->exportData(function ($entity, $csvService) use ($request) {
+
+                $Csvs = $csvService->getCsvs();
+
+                $Order = $entity;
+                $OrderItems = $Order->getOrderItems();
+
+                foreach ($OrderItems as $OrderItem) {
+                    $ExportCsvRow = new ExportCsvRow();
+
+                    // CSV出力項目と合致するデータを取得.
+                    foreach ($Csvs as $Csv) {
+                        // 受注データを検索.
+                        $ExportCsvRow->setData($csvService->getData($Csv, $Order));
+                        if ($ExportCsvRow->isDataNull()) {
+                            // 受注データにない場合は, 受注明細を検索.
+                            $ExportCsvRow->setData($csvService->getData($Csv, $OrderItem));
+                        }
+                        if ($ExportCsvRow->isDataNull() && $Shipping = $OrderItem->getShipping()) {
+                            // 受注明細データにない場合は, 出荷を検索.
+                            $ExportCsvRow->setData($csvService->getData($Csv, $Shipping));
+                        }
+
+                        $event = new EventArgs(
+                            [
+                                'csvService' => $csvService,
+                                'Csv' => $Csv,
+                                'OrderItem' => $OrderItem,
+                                'ExportCsvRow' => $ExportCsvRow,
+                            ],
+                            $request
+                        );
+                        $this->eventDispatcher->dispatch(EccubeEvents::ADMIN_ORDER_CSV_EXPORT_ORDER, $event);
+
+                        $ExportCsvRow->pushData();
+                    }
+
+                    //$row[] = number_format(memory_get_usage(true));
+                    // 出力.
+                     $csvService->fputcsv($ExportCsvRow->getRow());
+                }
+            });
+        });
+
+        $response->send();
+
+        log_info('[注文完了] 注文完了画面を表示しました. ', [$hasNextCart]);
+
+        return [
+            'Order' => $Order,
+            'hasNextCart' => $hasNextCart,
+        ];
+    }
+}
